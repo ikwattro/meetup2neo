e02d78f1d4100efaa6f63f4a28241a7cfbbebac6
Christophe Willemsen <willemsen.christophe@gmail.com>
2015-04-18 23:47:36 +0200

    meetup.html page generation

--- import.php
+++ import.php
@@ -33,7 +33,7 @@ $neoClient = ClientBuilder::create()
     )
     ->setAutoFormatResponse(true)
     ->build();
-
+/**
 // Creating Schema Indexes And Constraints
 if (!$skipSchemaSetup) {
     $neoClient->createUniqueConstraint('Event', 'id');
@@ -206,4 +206,98 @@ $p = [
 ];
 echo 'Inserting RSVPs' . "\n";
 $neoClient->sendCypherQuery($query, $p);
+
+
+// Building meetup.html d3 demo
+ */
+
+$template = '
+<!DOCTYPE html>
+<meta charset="utf-8">
+<style>
+text {
+    font: 10px sans-serif;
+}
+</style>
+<body>
+<script src="http://d3js.org/d3.v3.min.js"></script>
+<script>
+var diameter = 960,
+    format = d3.format(",d"),
+    color = d3.scale.category20c();
+
+var bubble = d3.layout.pack()
+    .sort(null)
+    .size([diameter, diameter])
+    .padding(1.5);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", diameter)
+    .attr("height", diameter)
+    .attr("class", "bubble");
+
+';
+
+$query = 'MATCH (event:Event {id: {event_id}})
+MATCH (event)<-[:PARTICIPATE]-(m)
+MATCH (m)-[:MEMBER_OF]->(group)<-[:TAGS_GROUP]-(t:Topic)
+WITH t.name as topics, count(*) as c
+WHERE c > 10
+RETURN topics, c';
+$p = ['event_id' => $eventId];
+$result = $neoClient->sendCypherQuery($query, $p)->getResult();
+$topics = $result->get('topics');
+$counts = $result->get('c');
+$i = 1;
+$template .= '
+var data = [';
+foreach ($topics as $k => $topic) {
+    $template .= '{"topic":"' . $topic . '", "count": ' . (int) $counts[$k] . '}';
+    if ($i < count($topics)) {
+        $template .= ',';
+    }
+    $i++;
+}
+$template .= '];';
+$template .= '
+var bobo = {};
+bobo.name = "interests";
+bobo.children = data;
+var bubble = d3.layout.pack().sort(null).size([960,960]).padding(1.5);
+var node = svg.selectAll(".node")
+        .data(bubble.nodes(classes(bobo)))
+               // .filter(function(d) { return !d.children; }))
+        .enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+node.append("title")
+        .text(function(d) { return d.className + ": " + format(d.value); });
+
+node.append("circle")
+        .attr("r", function(d) { return d.r; })
+        .style("fill", function(d) { return color(d.packageName); });
+
+node.append("text")
+        .attr("dy", ".3em")
+        .style("text-anchor", "middle")
+        .text(function(d) { var txt = undefined !== d.className ? d.className: "topic"; return txt.substring(0, d.r / 3); });
+
+
+// Returns a flattened hierarchy containing all leaf nodes under the root.
+function classes(root) {
+    var classes = [];
+
+    function recurse(name, node) {
+        if (node.children) node.children.forEach(function(child) { recurse(node.topic, child); });
+        else classes.push({packageName: node.topic, className: node.topic, value: node.count});
+    }
+
+    recurse(null, root);
+    return {children: classes};
+}
+
+d3.select(self.frameElement).style("height", diameter + "px");';
+$template .= '</script>';
+file_put_contents('meetup.html', $template);
 echo 'Import Done, Enjoy !' . "\n";
